4cf2884a63118bc445637ffb27fea7ac076e4c72
Georg Jaehnig <georg@jaehnig.org>
2017-03-15 19:35:58 +0100

    Use QR boilerplate.

--- index.android.js
+++ index.android.js
@@ -1,53 +1,54 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+'use strict';
+ 
+import React, { Component } from 'react'
+import QRCode from 'react-native-qrcode';
+ 
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    View,
+    TextInput
 } from 'react-native';
-
-export default class QrShow extends Component {
+ 
+class QrShow extends Component {
+  state = {
+    text: 'http://facebook.github.io/react-native/',
+  };
+ 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => this.setState({text: text})}
+          value={this.state.text}
+        />
+        <QRCode
+          value={this.state.text}
+          size={200} />
       </View>
     );
-  }
+  };
 }
-
+ 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+ 
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        margin: 10,
+        borderRadius: 5,
+        padding: 5,
+    }
 });
-
+ 
 AppRegistry.registerComponent('QrShow', () => QrShow);
+ 
+module.exports = QrShow;
