4e524a5c093dcc05bad2e2ee3a55f4223048d7ef
Georg Jaehnig <georg@jaehnig.org>
2017-03-24 19:35:57 +0100

    Align.

--- js/QrShow.js
+++ js/QrShow.js
@@ -4,17 +4,17 @@ import {
   StackNavigator,
 } from 'react-navigation';
 
-import MainScreen  from './MainScreen.js';
-import PhoneScreen from './PhoneScreen.js';
-import AddScreen   from './AddScreen.js';
-import TextScreen  from './TextScreen.js';
+import MainScreen     from './MainScreen.js';
+import PhoneScreen    from './PhoneScreen.js';
+import AddScreen      from './AddScreen.js';
+import TextScreen     from './TextScreen.js';
 import ContactScreen  from './ContactScreen.js';
 
 const QrShow = StackNavigator({
-  Main:  {screen: MainScreen},
-  Add:   {screen: AddScreen},
-  Phone: {screen: PhoneScreen},
-  Text:  {screen: TextScreen},
+  Main:    {screen: MainScreen},
+  Add:     {screen: AddScreen},
+  Phone:   {screen: PhoneScreen},
+  Text:    {screen: TextScreen},
   Contact: {screen: ContactScreen},
 }); 
 
