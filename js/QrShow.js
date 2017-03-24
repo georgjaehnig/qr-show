27219259f7c8d824182a44b944dbbfa2abbfb4b9
Georg Jaehnig <georg@jaehnig.org>
2017-03-24 14:00:32 +0100

    Add to app.

--- js/QrShow.js
+++ js/QrShow.js
@@ -7,11 +7,13 @@ import {
 import MainScreen  from './MainScreen.js';
 import PhoneScreen from './PhoneScreen.js';
 import AddScreen   from './AddScreen.js';
+import TextScreen  from './TextScreen.js';
 
 const QrShow = StackNavigator({
   Main:  {screen: MainScreen},
   Add:   {screen: AddScreen},
   Phone: {screen: PhoneScreen},
+  Text:  {screen: TextScreen},
 }); 
 
 module.exports = QrShow;
