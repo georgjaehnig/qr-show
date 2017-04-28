2fa87558253666f70acc2a5f8ec243871bbea540
Georg Jaehnig <georg@jaehnig.org>
2017-04-28 17:42:05 +0200

    Logs and comments.

--- js/codes.js
+++ js/codes.js
@@ -51,14 +51,47 @@ export default function reducer(state = initialState, action = {}) {
       nextState = {
         ...state  
       };
+      console.log('before slice', nextState.codes);
+
+      /*
+      nextState.codes = [
+        {
+          value: 'https://github.com/georgjaehnig/qr-show',
+          type:  'URL',
+          fields: {
+            description: 'QR Show on Github',
+            url: 'https://github.com/georgjaehnig/qr-show',
+          }
+        },
+        {
+          value: 'wifi:something',
+          type:  'Wifi',
+          fields: {
+            description: 'my-wifi',
+            ssid: 'ciferamondo',
+            password: 'somepass',
+            type: 'WPA',
+            hidden: false,
+          }
+        },
+      ];
+      */
+
+      /*
       nextState.codes = [
         ...state.codes.slice(0, state.currentCodeIndex),
         ...state.codes.slice(state.currentCodeIndex + 1),
       ];
+      */
+      nextState.codes = state.codes
+        .slice(0, state.currentCodeIndex)
+        .concat(state.codes.slice(state.currentCodeIndex + 1));
+
+
+      console.log('after slice', nextState.codes);
       // Limit index to array length.  
       nextState.currentCodeIndex = Math.min(state.currentCodeIndex, state.codes.length - 1);
-      console.log('state', state);
-      console.log('nextState', nextState);
+      //nextState.currentCodeIndex = 1;
       return nextState;
     case 'setCurrentCodeIndex':
       nextState = {
